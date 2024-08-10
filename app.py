05d31c752fc12b87936bf94a355a1f4c4847f292
Urban <elenapolevschikova75@gmail.com>
2024-08-10 14:46:05 +0300

    Flask + 1

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sl
 from datetime import datetime
 
+
 app = Flask(__name__)  # мы созд. объект на основе класса Flask
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'   # здесь по ключу делаем подсоединение к ДБ 'sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'  # здесь по ключу делаем подсоединение к ДБ 'sqlite'
 db = SQLAlchemy(app)   # здесь мы передаём сам объект(db) к нашему проекту
 
 
 class Article(db.Model):  # создаём класс для хранения записей с полями в определённой таблице
-    id = db.Column(db.Integer, primary_key=True)   # Integer - только числа, primary_key - уникальное id
-    title = db.Column(db.String(100), nullable=False)  # title - название, нельзя устанавливат пустым
-    intro = db.Column(db.String(300), nullable=False)
+    id = sl.Column(sl.Integer, primary_key=True)   # Integer - только числа, primary_key - уникальное id
+    type = sl.Column(sl.String)
+    title = sl.Column(sl.String(100), nullable=False)  # title - название, нельзя устанавливат пустым
+    intro = sl.Column(sl.String(300), nullable=False)
     # intro - вступление, String(300) - текст, длинна, nullable=False - не вступать без вступления
-    text = db.Column(db.Text, nullable=False)   # text - для установки полного текста
-    date = db.Column(db.DateTime, default=datetime.utcnow)  # date - дата публикаций, utcnow - время публикации
+    text = sl.Column(sl.Text, nullable=False)   # text - для установки полного текста
+    date = sl.Column(sl.DateTime, default=datetime.utcnow)  # date - дата публикаций, utcnow - время публикации
+    with app.app_context():
+        db.create_all()
 
 
     def __repr__(self):  # при поиске инфо, метод выдачи объекта с id
         return '<Article %r' % self.id
-
 
 
 @app.route("/")  # если мы хотим отслеживать главн. стр. вывод.текст - указываем /(слеш)
@@ -33,10 +37,16 @@ def about():  # выводим функцию
     return render_template("about.html")  # возврат
 
 
-@app.route("/user/<string:name>/<int:id>")  # если надо получить некий параметр из url адр.(/user,/name имя,/id инд)
-def user(name, id):  # выводим функцию
-    return "User page:" + name + " - " + str(id)     # возврат
+@app.route("/create_article")  #
+def create_article():  # выводим функцию
+    return render_template("create_article.html")  # возврат
+
+
+# @app.route("/user/<string:name>/<int:id>")  # если надо получить некий параметр из url адр.(/user,/name имя,/id инд)
+# def user(name, id):  # выводим функцию
+#     return "User page:" + name + " - " + str(id)     # возврат
 
 
 if __name__ == "__main__":   # если мы будем выводить в этом файле main
-    app.run(debug=True)      # обращ. к объекту app далее к команде run(запуск этого проекта)
+    app.run(debug=True)   # обращ. к объекту app далее к команде run(запуск этого проекта)
+
